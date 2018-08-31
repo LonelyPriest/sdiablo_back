6c336313d3d40790a715ad0a9e11b9872bca4e95
buxianhui <47396648@qq.com>
2018-08-31 22:10:05 +0800

    fit bugs

--- plugins-src/diablo/diablo_controller/src/diablo_import_from_csv.erl
+++ plugins-src/diablo/diablo_controller/src/diablo_import_from_csv.erl
@@ -71,7 +71,7 @@ insert_into_member(Merchant, Datetime, Time, [H|T], Sort, Acc) ->
     {RName, Phone, Shop, Score, Consume, Birth, Date} = H,
     ?DEBUG("H ~p", [H]),
     NewShop = case Shop of
-		  <<>> -> 129;
+		  <<>> -> 143;
 		  _ -> Shop
 	      end,
     NewScore = case Score of
