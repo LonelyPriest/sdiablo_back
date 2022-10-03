b9b591218f656ea7421a2eb3b368c9f7187dd6f0
buxianhui <47396648@qq.com>
2022-10-03 21:42:19 +0800

    fit bugs

--- plugins-src/diablo/diablo_controller/hdoc/private/wsale/js/wsale_utils.js
+++ plugins-src/diablo/diablo_controller/hdoc/private/wsale/js/wsale_utils.js
@@ -849,21 +849,26 @@ var wsaleUtils = function(){
 
 	check_retailer_barcode: function(barcode) {
 	    o_barcode = parseInt(barcode, 16).toString();
-	    console.log(o_barcode);
-	    var year   = o_barcode.substring(0, 2);
-	    var month  = wsaleUtils.to_integer(o_barcode.substring(2, 4));
-	    var date   = wsaleUtils.to_integer(o_barcode.substring(4, 6));
-	    var hour   = wsaleUtils.to_integer(o_barcode.substring(6, 8));
-	    var minute = wsaleUtils.to_integer(o_barcode.substring(8, 10));
+	    // console.log(o_barcode);
+	    if (o_barcode.length === 12) {
+		var year   = o_barcode.substring(0, 2);
+		var month  = wsaleUtils.to_integer(o_barcode.substring(2, 4));
+		var date   = wsaleUtils.to_integer(o_barcode.substring(4, 6));
+		var hour   = wsaleUtils.to_integer(o_barcode.substring(6, 8));
+		var minute = wsaleUtils.to_integer(o_barcode.substring(8, 10));
 
-	    var t2 = new Date();
-	    var t2_year = t2.getFullYear();
-	    var full_year = wsaleUtils.to_integer(t2_year.toString().substring(0, 2) + year);
+		var t2 = new Date();
+		var t2_year = t2.getFullYear();
+		var full_year = wsaleUtils.to_integer(t2_year.toString().substring(0, 2) + year);
+		
+		var t1 =  new Date(full_year, month - 1, date, hour, minute); 
+		console.log(t1);
+		// 5 minute
+		return t2.getTime() - t1.getTime() <= 5 * 1000 * 60; 
+	    }
+
+	    return false;
 	    
-	    var t1 =  new Date(full_year, month - 1, date, hour, minute); 
-	    console.log(t1);
-	    // 5 minute
-	    return t2.getTime() - t1.getTime() <= 5 * 1000 * 60; 
 	}
 	
 	// 
